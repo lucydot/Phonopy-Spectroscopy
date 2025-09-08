cdeb31abdbb37b797707bed53eb606fe3174c0c2
Lucy Whalley <l.whalley@northumbria.ac.uk>
2025-09-08 16:16:41 +0100

    packaging files

--- lib/phonopy_spectroscopy/__init__.py
+++ lib/phonopy_spectroscopy/__init__.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("phonopy_spectroscopy")
+except PackageNotFoundError:
+    # If the package is not installed, don't add __version__
+    pass
